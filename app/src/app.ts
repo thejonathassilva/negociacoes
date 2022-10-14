0124a9da08a7ca1b19258d2eb156173711e847cd
Jonathas Silva <jonathas_alves5@hotmail.com>
2022-10-14 14:46:58 -0300

    finalização de negociações

--- app/src/app.ts
+++ app/src/app.ts
@@ -1,4 +1,5 @@
 import { NegociacaoController } from './controllers/negociacao-controller.js';
+import { domInjector } from './decorators/dom-injector.js';
 
 const controller = new NegociacaoController();
 const form = document.querySelector('.form');
@@ -9,4 +10,13 @@ if (form) {
     });
 } else {
     throw Error('Não foi possível inicializar a aplicação. Verifique se o form existe.');
+}
+
+const botaoImporta = document.querySelector('#botao-importa');
+if (botaoImporta) {
+    botaoImporta.addEventListener('click', () => {
+        controller.importaDados();
+    });
+} else {
+    throw Error('Botão importa não foi encontrado')
 }
